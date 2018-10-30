2cbcb7c7b01d2ba3ecff445d03c3e5c186fc512e
Romain Marca <rmarca@hotmail.fr>
2018-10-30 16:00:14 +0100

    repair

--- app/src/main/java/fr/wildcodeschool/candyhunt/GameArenaActivity.java
+++ app/src/main/java/fr/wildcodeschool/candyhunt/GameArenaActivity.java
@@ -5,14 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 
 import cn.iwgang.countdownview.CountdownView;
 
-public class GameArena extends AppCompatActivity{
+public class GameArenaActivity extends AppCompatActivity{
 
     int timeDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_arena);
+        setContentView(R.layout.game_arena_activity);
 
         CountdownView countDownTimer = findViewById(R.id.countdown);
         countDownTimer.start(timeDuration);

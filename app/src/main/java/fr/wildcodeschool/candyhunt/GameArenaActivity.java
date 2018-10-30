7fb57c418771e91f55993fea26e619639e4d3b48
Pierre Fajardo <pierrefaja@gmail.com>
2018-10-30 17:34:54 +0100

    je sais pas quoi écrire

--- app/src/main/java/fr/wildcodeschool/candyhunt/GameArenaActivity.java
+++ app/src/main/java/fr/wildcodeschool/candyhunt/GameArenaActivity.java
@@ -3,6 +3,8 @@ package fr.wildcodeschool.candyhunt;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 import cn.iwgang.countdownview.CountdownView;
 
 public class GameArenaActivity extends AppCompatActivity{
@@ -13,6 +15,13 @@ public class GameArenaActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_arena_activity);
+
+        ArrayList<Player> players = new ArrayList<>();
+        Player player = new Player(0,"player_one");
+        players.add(player);
+
+
+        GameDieHard dieHard = new GameDieHard(true,1,players,50);
 
         CountdownView countDownTimer = findViewById(R.id.countdown);
         countDownTimer.start(timeDuration);

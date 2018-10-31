006ce7d06a68105b43c7071e26809af080b93e64
Romain Marca <rmarca@hotmail.fr>
2018-10-31 07:09:32 +0100

    menu

--- app/src/main/java/fr/wildcodeschool/candyhunt/DeadActivity.java
+++ app/src/main/java/fr/wildcodeschool/candyhunt/DeadActivity.java
@@ -1,7 +1,9 @@
 package fr.wildcodeschool.candyhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DeadActivity extends AppCompatActivity {
@@ -14,5 +16,14 @@ public class DeadActivity extends AppCompatActivity {
 
         TextView score = findViewById(R.id.score);
         score.setText("Votre score :" + Singleton.getInstance().getIndex());
+
+        TextView retour = findViewById(R.id.close);
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToMenu = new Intent(DeadActivity.this , MainActivity.class);
+                DeadActivity.this.startActivity(goToMenu);
+            }
+        });
     }
 }
